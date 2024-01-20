78d2d04c1b7531ac454a26709c848caf284fc53e
Ela <ela.latosinska@gmail.com>
2024-01-20 16:15:44 +0100

    implemented IngredientsHelper.java

--- src/helpers/IngredientsHelper.java
+++ src/helpers/IngredientsHelper.java
@@ -7,13 +7,6 @@ import java.util.List;
 import java.util.Objects;
 
 public class IngredientsHelper {
-    public Ingredient convertStringToIngredient(String ingredientString) {
-        String[] ingredientFields = ingredientString.split(",");
-        Ingredient ingredient = new Ingredient();
-        ingredient.setName(ingredientFields[0]);
-        ingredient.setVegetarian(Objects.equals(ingredientFields[1], "vege"));
-        return ingredient;
-    }
 
     public List<Ingredient> convertStringToListOfIngredients(String ingredientsString) {
         List<Ingredient> ingredients = new ArrayList<>();
@@ -22,5 +15,28 @@ public class IngredientsHelper {
             ingredients.add(convertStringToIngredient(ingredient));
         }
         return ingredients;
+    }
+
+    public String convertListOfIngredientsToString(List<Ingredient> ingredients) {
+        StringBuilder sb = new StringBuilder();
+        for(var ingredient : ingredients) {
+            sb.append(convertIngredientToString(ingredient)).append(";");
+        }
+        return sb.toString();
+    }
+
+    private String convertIngredientToString(Ingredient ingredient) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(ingredient.getName());
+        sb.append(ingredient.isVegetarian() ? "vege" : "meat");
+        return sb.toString();
+    }
+
+    private Ingredient convertStringToIngredient(String ingredientString) {
+        String[] ingredientFields = ingredientString.split(",");
+        Ingredient ingredient = new Ingredient();
+        ingredient.setName(ingredientFields[0]);
+        ingredient.setVegetarian(Objects.equals(ingredientFields[1], "vege"));
+        return ingredient;
     }
 }
