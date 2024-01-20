8d1566f8c9efa2dec804d4cfbf71c9efa809026f
Ela <ela.latosinska@gmail.com>
2024-01-20 17:25:41 +0100

    added new size option

--- src/model/Size.java
+++ src/model/Size.java
@@ -1,8 +1,11 @@
 package model;
 
 public enum Size {
-    SMALL(1), MEDIUM(1.2), LARGE(1.5);
+    SMALL(1), MEDIUM(1.2), LARGE(1.5), GIANT(2.0);
+
+    public final double priceMultiplier;
 
     Size(double priceMultiplier) {
+        this.priceMultiplier = priceMultiplier;
     }
 }
